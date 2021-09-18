b8866c9d647f2ac6b0586694694d571e220734be
Siva Sekhar Reddy <sivasekhar2@gmail.com>
2021-09-18 05:45:03 +0530

    first commit

---
+++ src/main/java/com/example/demo/HealthCareProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareProjectApplication
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(HealthCareProjectApplication.class, args);
+		System.out.println("Main Method is Executed");
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/Specialization.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="Specialization_tab")
+public class Specialization 
+{
+  @Id
+  @Column(name="spec_id_col")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+  
+   @Column(name="spec_code_col")
+   private String specCode;
+   
+   @Column(name="spec_name_col")
+   private String specName;
+   
+   @Column(name="spec_note_col")
+   private String specNote;
+}
